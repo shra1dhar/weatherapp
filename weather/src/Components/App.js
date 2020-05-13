75d2d79b1ce7b9d8a17f9a464206901e89d9a604
devashrlucas <devashrlucas@gmail.com>
2020-05-13 15:29:47 -0400

    Started 'FetchWeather' as a function component with hooks.

--- weather/src/Components/App.js
+++ weather/src/Components/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/App.css';
+import FetchWeather from './FetchWeather';
+
+require("dotenv").config();
 
 function App() {
+  const [weather, setWeather] = useState("");
+
   return (
     <div className="App">
+      <FetchWeather zipcode={(console.warn)}/>
     </div>
   );
 }
