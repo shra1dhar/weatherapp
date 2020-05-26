7e77d1057d8284f54c9c42b1869d9a943a414528
devashrlucas <devashrlucas@gmail.com>
2020-05-26 13:49:03 -0400

    Working on Promise.all() and .forEach() to update object in App state.

--- weather/src/Components/App.js
+++ weather/src/Components/App.js
@@ -8,7 +8,23 @@ class App extends React.Component {
     this.state = { 
       weather: { hourly: "", fiveDay: ""}, }; 
 
-    //this.componentDidMount = this.componentDidMount.bind(this); //solution to fix this.setState. "this" was
+    this.componentDidMount = this.componentDidMount.bind(this); //solution to fix this.setState. "this" was
+  }
+
+  componentDidMount(zipcode){
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const country = "us";
+    const urls = [
+      `https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},${country}&units=imperial&appid=${API_KEY}`,
+      `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},${country}&unit=imperial&appid=${API_KEY}`,
+    ];
+
+    const requests = urls.map(url => fetch(url));
+
+    Promise.all(requests)
+      .then(responses => responses.forEach(
+        response => this.setState({ weather: { hourly: `${response}`, fiveDay: `${response}`}})
+      ));
   }
 /*
   componentDidMount(zipcode) {
