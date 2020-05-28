933958d8682bd7df3c82a0a54b98682b7d6b9c5d
devashrlucas <devashrlucas@gmail.com>
2020-05-28 09:52:44 -0400

    Trying another way to map Promise.all() to App state. Returned object data can be viewed in console using index position.

--- weather/src/Components/App.js
+++ weather/src/Components/App.js
@@ -14,13 +14,20 @@ class App extends React.Component {
   componentDidMount(zipcode){
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
     const country = "us";
-    const test = [];
     const urls = [
       `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},${country}&unit=imperial&appid=${API_KEY}`, //hourly
       `https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},${country}&units=imperial&appid=${API_KEY}` // five day
     ];
     
-    const requests = Promise.all(urls.map(url => fetch(url)));
+    const requests = Promise.all(urls.map(url => fetch(url))).then(function (responses) { 
+      // Get a JSON object from each of the responses
+      return responses.map(function (response) {
+        return response.json();
+      });
+    }).then(function (data) {
+      //Update App state with data from responses
+      console.log(data[0]);
+    })
     //const resp = requests.map((x, xi) => (this.setState({ weather: { hourly: x, fiveDay: xi}})));
   } 
 /*
