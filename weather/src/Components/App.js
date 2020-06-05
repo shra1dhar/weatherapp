90ac048c6396155f64a99b31af396fa96465c36f
devashrlucas <devashrlucas@gmail.com>
2020-06-05 16:08:44 -0400

    Trying async/await with map and Promise.all() again.

--- weather/src/Components/App.js
+++ weather/src/Components/App.js
@@ -6,14 +6,15 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      weather: {},
+      hourly: [],
+      fiveDay: [],
     };
 
     this.componentDidMount = this.componentDidMount.bind(this); //solution to fix this.setState. "this" was
   }
 
   componentDidMount(zipcode) {
-    const reference = this; //need to cache reference to "this" (App object) for "this.setState" outside of callback to fetch request
+    const that = this; //need to cache reference to "this" (App object) for "this.setState" outside of callback to fetch request
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
     const country = "us";
     const urls = [
@@ -21,24 +22,30 @@ class App extends React.Component {
       `https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},${country}&units=imperial&appid=${API_KEY}`, // five day
     ];
 
+    const requests = async () => {
+      const fetchCalls = await Promise.all([
+      fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},${country}&unit=imperial&appid=${API_KEY}`),
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},${country}&units=imperial&appid=${API_KEY}`)])
+      try {
+        let responseJson = await fetchCalls.json();
+      } catch (err) {
+        alert(err)
+      }
+    }
+/*
     const requests = Promise.all(urls.map((url) => fetch(url)))
       .then(function(responses) {
         // Get a JSON object from each of the responses
         return responses.map(function(response) {
           return response.json();
         });
-      }).then(function(data) {
+      }).then(data => {
         //Update App state with data from responses
-        //await console.log(typeof(data))
-        for (let entry of Object.entries(data)) {
-          for(let x of entry) {
-            //console.log(typeof(x))
-            console.log(x)
-            }
-          }
-        });
-      }
-
+         console.log(data);
+       //that.setState({hourly : data["0"], fiveDay: data["1"]});
+          });
+  */
+  }
 
   render() {
     return (
