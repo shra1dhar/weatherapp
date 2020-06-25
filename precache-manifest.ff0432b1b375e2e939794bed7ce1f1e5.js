47d1499cc75ba8c409dde9ae9f7d4d3902fa671e
devashrlucas <devashrlucas@gmail.com>
2020-06-25 16:46:44 -0400

    Updates

---
+++ precache-manifest.ff0432b1b375e2e939794bed7ce1f1e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0063b8ecfe89fc20f21889d4c0e27c11",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "185b93502937c63dd9ff",
+    "url": "/weatherapp/static/css/main.231760c6.chunk.css"
+  },
+  {
+    "revision": "c5f656025100bed9ac4a",
+    "url": "/weatherapp/static/js/2.e3e0e26b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.e3e0e26b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "185b93502937c63dd9ff",
+    "url": "/weatherapp/static/js/main.a48fb750.chunk.js"
+  },
+  {
+    "revision": "97994ece5cf1ba8fd757",
+    "url": "/weatherapp/static/js/runtime-main.80ae9ae2.js"
+  },
+  {
+    "revision": "fa3bc85849ea74cf423cba0e7286b1f7",
+    "url": "/weatherapp/static/media/cloud.fa3bc858.png"
+  },
+  {
+    "revision": "fab113ba9a48a29f2da3486b8f67e66c",
+    "url": "/weatherapp/static/media/drizzle.fab113ba.png"
+  },
+  {
+    "revision": "201ea217fa162cabf5b4b99d7fa1611c",
+    "url": "/weatherapp/static/media/lightning-rain.201ea217.png"
+  },
+  {
+    "revision": "f7da4a04d31cf1449f2047852ba859d8",
+    "url": "/weatherapp/static/media/rain.f7da4a04.png"
+  },
+  {
+    "revision": "19176fa774126ae0e06bcdbdfa8338dd",
+    "url": "/weatherapp/static/media/snow.19176fa7.png"
+  },
+  {
+    "revision": "9a77a9b204ac9025cec4bfa609285183",
+    "url": "/weatherapp/static/media/sun.9a77a9b2.png"
+  }
+]);
