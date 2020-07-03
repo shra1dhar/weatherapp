3a7c0eb667110b633df4a9573165a141c4a5b24e
devashrlucas <devashrlucas@gmail.com>
2020-07-03 12:05:31 -0400

    Test for error message finished.

--- weather/src/Tests/App.test.js
+++ weather/src/Tests/App.test.js
@@ -1,4 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './Components/App';
+import { render, cleanup, screen, fireEvent, waitFor } from '@testing-library/react';
+
+import App from '../Components/App';
+
+afterEach(() => {
+    cleanup()
+});
+
+describe('App', () => {
+    test('renders App component', () => {
+        render(<App />)
+    }); 
+
+    test('search returns an error message', () => {
+        const { getByPlaceholderText } = render(<App />);
+        jest.spyOn(window, "alert").mockImplementation(() => {});
+        
+        expect(getByPlaceholderText(/Enter/i).textContent).toBe("")
+        fireEvent.submit(getByPlaceholderText(/Enter/i), {target: {value: "1"}})
+        fireEvent.keyDown(getByPlaceholderText(/Enter/i), {key: "Enter", code: "Enter"});
+        expect(window.alert).toHaveBeenCalledWith("Please enter a 5 digit zipcode.");
+    });
+
+});
+
 
